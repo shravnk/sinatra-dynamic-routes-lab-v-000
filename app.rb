cad4783c21f00014cd1c65bef87fd413093c2b19
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-20 21:23:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,36 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name'
+    name = params[:name]
+    "#{name.reverse}"
+  end
 
+  get '/square/:number'
+    num = params[:number]
+    "#{num^2}"
+  end
+
+  get '/say/:number/:phrase'
+    num = params[:number]
+    phrase = params[:phrase]
+    num.times do
+      "#{phrase}"
+    end    
+    
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5'
+    word1 = params[:word1]
+    word2 = params[:word2]
+    word3 = params[:word3]
+    word4 = params[:word4]
+    word5 = params[:word5]
+    "#{word1} #{word2} #{word3} #{word4} #{word5}"
+  end
+
+  get '/'
+    = params[]
+    "#{}"
+  end
 end
